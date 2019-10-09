c494bdd75198e78fdf3c5a26a6ab4b272944eae6
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-09 19:43:33 +0530

    Update Apivendor.php

--- application/controllers/Apivendor.php
+++ application/controllers/Apivendor.php
@@ -1659,35 +1659,7 @@ class Apivendor extends CI_Controller
 
     //-----------------------------------------------//
 
-    	public function list_completed_services()
-    	{
-    	   $_POST = json_decode(file_get_contents("php://input"), TRUE);
 
-    		if(!$this->checkMethod())
-    		{
-    			return FALSE;
-    		}
-
-    		if($_POST == FALSE)
-    		{
-    			$res = array();
-    			$res["opn"] = "List Completed services";
-    			$res["scode"] = 204;
-    			$res["message"] = "Input error";
-
-    			echo json_encode($res);
-    			return;
-    		}
-    		$user_master_id = '';
-
-    		$user_master_id  = $this->input->post("user_master_id");
-
-    		$data['result']=$this->apivendormodel->List_completed_services($user_master_id);
-    		$response = $data['result'];
-    		echo json_encode($response);
-    	}
-
-    //-----------------------------------------------//
 
 
     //-----------------------------------------------//
