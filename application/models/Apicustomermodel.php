3448b80d70334f6cbda1122a4d7c86edf81a0f7d
kamalhappysanz <kamal.happysanz@gmail.com>
2019-10-10 11:27:30 +0530

    sms version
    checked

--- application/models/Apicustomermodel.php
+++ application/models/Apicustomermodel.php
@@ -28,59 +28,46 @@ class Apicustomermodel extends CI_Model {
 
 	 function sendSMS($Phoneno,$Message)
 	{
-        //Your authentication key
-        $authKey = "191431AStibz285a4f14b4";
+      $curl = curl_init();
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.msg91.com/api/v2/sendsms?country=91",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => '{
+                    "sender": "TTNULM",
+                    "route": "4",
+                    "country": "91",
+                    "sms": [
+                    {
+                      "message": "'.$Message.'",
+                      "to": [
+                      "'.$Phoneno.'"
+                      ]
+                    }
+                    ]
+                  }',
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_HTTPHEADER => array(
+          "authkey: 191431AStibz285a4f14b4",
+          "content-type: application/json"
+        ),
+      ));
 
-        //Multiple mobiles numbers separated by comma
-        $mobileNumber = "$Phoneno";
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
 
-        //Sender ID,While using route4 sender id should be 6 characters long.
-        $senderId = "T-NULM";
+      curl_close($curl);
 
-        //Your message to send, Add URL encoding here.
-        $message = urlencode($Message);
-
-        //Define route
-        $route = "transactional";
-
-        //Prepare you post parameters
-        $postData = array(
-            'authkey' => $authKey,
-            'mobiles' => $mobileNumber,
-            'message' => $message,
-            'sender' => $senderId,
-            'route' => $route
-        );
-
-        //API URL
-        $url="https://control.msg91.com/api/sendhttp.php";
-
-        // init the resource
-        $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $postData
-            //,CURLOPT_FOLLOWLOCATION => true
-        ));
-
-
-        //Ignore SSL certificate verification
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-
-        //get response
-        $output = curl_exec($ch);
-
-        //Print error if any
-        if(curl_errno($ch))
-        {
-            echo 'error:' . curl_error($ch);
-        }
-
-        curl_close($ch);
+      if ($err) {
+        echo "cURL Error #:" . $err;
+      } else {
+        // echo $response;
+      }
 	}
 
 //-------------------- SMS End -------------------//
--- application/models/Smsmodel.php
+++ application/models/Smsmodel.php
@@ -10,10 +10,11 @@ Class Smsmodel extends CI_Model
   }
 
 
-   function send_sms($phone,$notes)
+   function send_sms_1($phone,$notes)
   	{
           //Your authentication key
           $authKey = "191431AStibz285a4f14b4";
+
 
           //Multiple mobiles numbers separated by comma
           $mobileNumber = "$phone";
@@ -67,6 +68,55 @@ Class Smsmodel extends CI_Model
 
           curl_close($ch);
   	}
+
+
+
+    function send_sms($phone,$notes)
+    {
+
+      $curl = curl_init();
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.msg91.com/api/v2/sendsms?country=91",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => '{
+      						  "sender": "TTNULM",
+      						  "route": "4",
+      						  "country": "91",
+      						  "sms": [
+      							{
+      							  "message": "'.$notes.'",
+      							  "to": [
+      								"'.$phone.'"
+      							  ]
+      							}
+      						  ]
+      						}',
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_HTTPHEADER => array(
+          "authkey: 191431AStibz285a4f14b4",
+          "content-type: application/json"
+        ),
+      ));
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+
+      curl_close($curl);
+
+      if ($err) {
+        echo "cURL Error #:" . $err;
+      } else {
+        // echo $response;
+      }
+
+
+    }
 
 
 
